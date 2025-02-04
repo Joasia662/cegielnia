586449aa78fc5f23e76f3a0d5640cc2edb356a69
Joanna Cepuch <asiacepuch1@gmail.com>
2025-02-04 23:55:02 +0100

    fix

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ int worker_pids[MAX_WORKERS];
 int truck_pids[MAX_TRUCKS];
 
 void stop_workers(int signo) {
+    (void)signo; // Ignorowanie parametru, aby uniknąć ostrzeżenia
+
     for (int i = 0; i < MAX_WORKERS; i++) {
         if (worker_pids[i] > 0) {
             kill(worker_pids[i], SIGTERM);
@@ -45,7 +47,7 @@ int main() {
     }
 
     // Start truck processes
-    for (int i = 0; i < params.truck_count; i++) {
+    for (size_t i = 0; i < params.truck_count; i++) { // Poprawione i jako size_t
         if ((truck_pids[i] = fork()) == 0) {
             execl("./truck", "truck", NULL);
             perror("Error starting truck");
@@ -57,7 +59,7 @@ int main() {
     for (int i = 0; i < MAX_WORKERS; i++) {
         waitpid(worker_pids[i], NULL, 0);
     }
-    for (int i = 0; i < params.truck_count; i++) {
+    for (size_t i = 0; i < params.truck_count; i++) { // Poprawione i jako size_t
         waitpid(truck_pids[i], NULL, 0);
     }
 
